6c60a941285037f94abd26c324648fbe2c784957
Jan-Rienk Hemminga <janrienk@gmail.com>
2016-05-21 15:34:06 +0200

    edit DATA_DIR +cmd args

--- markdown_editor/main.py
+++ markdown_editor/main.py
@@ -1,18 +1,27 @@
-import os, shutil
+import os, shutil, getopt, sys
 
 DEBUG = True
+DATA_DIR = "../data"
 
 
-def main(dataDir):
+def parseArgs():
+    global DATA_DIR
+    opts, args = getopt.getopt(sys.argv[1:], '', ["data-dir="])
+    for opt, arg in opts:
+        if opt == "--data-dir":
+            DATA_DIR = arg
+            
+            
+def main():
     if DEBUG:
-        if os.path.isdir(dataDir):
+        if os.path.isdir(DATA_DIR):
             # cleanup test data
-            shutil.rmtree(dataDir)
-            main(dataDir)
+            shutil.rmtree(DATA_DIR)
+            return main(DATA_DIR)
         else:
             # setup test data
-            os.mkdir(dataDir)
-            os.chdir(dataDir)
+            os.mkdir(DATA_DIR)
+            os.chdir(DATA_DIR)
             open("testfile", mode='w')
 
 if __name__ == '__main__':
@@ -24,5 +33,5 @@ if __name__ == '__main__':
       \/_/  \/_/   \/____/      \/_____/   \/____/   \/_/     \/_/   \/_____/   \/_/ /_/
     License: LGPL v2.1
     """)
-    dataDir = "../data"
-    main(dataDir)
+    parseArgs()
+    main()
