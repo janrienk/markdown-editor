a104c515e157372a7ff3c00003f7cdc481f9b373
Jan-Rienk Hemminga <janrienk@gmail.com>
2016-05-21 18:27:33 +0200

    git flow test -->version +1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 if __name__ == "__main__":
     setup(name = "markdown-editor",
-          version = "0.0.1",
+          version = "0.0.2",
           license = "LGPLv2.1",
           author = "Jan Rienk Hemminga",
           author_email = "janrienk@gmail.com",
